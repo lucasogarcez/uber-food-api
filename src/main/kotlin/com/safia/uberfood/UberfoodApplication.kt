7db3abf12ef7e2d8a86426f40e977054c4eaa04c
Lucas Oliveira Garcez <estudosengcomputacao@gmail.com>
2024-05-08 07:44:09 -0300

    Add spring initializr

---
+++ src/main/kotlin/com/safia/uberfood/UberfoodApplication.kt
@@ -0,0 +1,11 @@
+package com.safia.uberfood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UberfoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<UberfoodApplication>(*args)
+}
---
+++ src/test/kotlin/com/safia/uberfood/UberfoodApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.safia.uberfood
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UberfoodApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
