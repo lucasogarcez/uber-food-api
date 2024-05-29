ce73186fca7e208392ae9ce0664c20fc96267be7
Pedro Rabis <rabis.pedro@gmail.com>
2024-05-29 09:14:36 -0300

    feat: add java project

---
+++ src/main/java/com/safia/uberfood/UberfoodApplication.java
@@ -0,0 +1,11 @@
+package com.safia.uberfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class UberfoodApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UberfoodApplication.class, args);
+	}
+}
